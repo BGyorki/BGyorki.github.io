22be26da287693c2cbb99522ecce3bef9fc7d598
BGyorki <brendan.gyorki@stu.cvccworks.edu>
2025-04-10 12:56:12 -0400

    Add files via upload

--- scripts/siteMap.js
+++ scripts/siteMap.js
@@ -5,9 +5,9 @@
 //have the ul on each page, add the li, use beforeEnd
 
 let siteMapLinks = ["index.html","about.html","projects.html","projects.html#chapter3","projects.html#chapter4","projects.html#chapter5","projects.html#chapter6",
-    "projects.html#chapter7","projects.html#chapter8","projects.html#chapter9","projects.html#chapter10","projects.html#js-Chapter3","misc.html","contact.html","changelog.html"];
+    "projects.html#chapter7","projects.html#chapter8","projects.html#chapter9","projects.html#chapter10","projects.html#js-Chapter3","projects.html#js-Chapter4","misc.html","contact.html","changelog.html"];
 let siteMapNames = ["index","about","projects","chapter 3","chapter 4","chapter 5","chapter 6",
-    "chapter 7","chapter 8","chapter 9","chapter 10","js chapter 3","misc","contact","changelog"];
+    "chapter 7","chapter 8","chapter 9","chapter 10","js chapter 3","js chapter 4","misc","contact","changelog"];
 
 window.addEventListener("load", addSiteMap);
 
