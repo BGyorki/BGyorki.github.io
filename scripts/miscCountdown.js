8f15b425eb018204b6a5adb2a1b1c042f0fabedf
BGyorki <brendan.gyorki@stu.cvccworks.edu>
2025-04-24 14:11:49 -0400

    Add files via upload

---
+++ scripts/miscCountdown.js
@@ -0,0 +1,22 @@
+var countDownDate = new Date("May 5, 2025 14:30:00").getTime();
+
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+
+    if(distance < 60 && distance > 0)
+        document.getElementById("countdownText").innerHTML = "yippee";
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementsByTagName("html")[0].innerHTML = "";
+    }
+}, 1000);
+
+
