8ee4e3dd23a5ba5f66c9e0aa9f19526f03c4eb77
BGyorki <brendan.gyorki@stu.cvccworks.edu>
2025-05-01 13:57:17 -0400

    Add files via upload

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -4,15 +4,10 @@ var x = setInterval(function(){
     var now = new Date().getTime();
     var distance = countDownDate - now;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
     if(distance < 0){
         clearInterval(x);
-        document.getElementsByTagName("html")[0].innerHTML = "";
+        document.getElementsByTagName("html")[0].innerHTML = end;
     }
-}, 1000);
+}, 250);
 
 
---
+++ scripts/miscCD.js
@@ -0,0 +1,26 @@
+var countDownDate = new Date("May 5, 2025 14:30:00").getTime();
+
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    console.log(distance + " " + days + " " + hours + " " + minutes + " " + seconds);
+
+    document.getElementById("countdown").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+
+    
+    
+
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementsByTagName("html")[0].innerHTML = end;
+    }
+}, 250);
+
+
